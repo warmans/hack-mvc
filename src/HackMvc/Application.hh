14aa91ce867909270f9b00fe701fff3b0c163867
warmans <stefan.warman@switchconcepts.com>
2014-03-23 13:01:23 +0000

    Added addtional HTTP verb handler functions

--- src/HackMvc/Application.hh
+++ src/HackMvc/Application.hh
@@ -8,10 +8,72 @@ class Application {
     public function __construct(private ?Service\Locator $service_locator)
     {}
 
+    /**
+    * HTTP GET Handler
+    */
     public function get($regex, callable $handler) {
         $this->routes[] = new Routing\RegexRoute(array('GET'), $regex, $handler);
     }
 
+    /**
+    * HTTP POST Handler
+    */
+    public function post($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('POST'), $regex, $handler);
+    }
+
+    /**
+    * HTTP PUT Handler
+    */
+    public function put($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('PUT'), $regex, $handler);
+    }
+
+    /**
+    * HTTP DELETE Handler
+    */
+    public function delete($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('DELETE'), $regex, $handler);
+    }
+
+    /**
+    * HTTP OPTIONS Handler
+    */
+    public function options($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('OPTIONS'), $regex, $handler);
+    }
+
+    /**
+    * HTTP HEAD Handler
+    */
+    public function head($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('HEAD'), $regex, $handler);
+    }
+
+    /**
+    * HTTP TRACE Handler
+    */
+    public function trace($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('TRACE'), $regex, $handler);
+    }
+
+    /**
+    * HTTP CONNECT Handler
+    */
+    public function connect($regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute(array('CONNECT'), $regex, $handler);
+    }
+
+    /**
+    * Handler for other/multiple HTTP verbs
+    */
+    public function handler(array<string> $methods, $regex, callable $handler) {
+        $this->routes[] = new Routing\RegexRoute($methods, $regex, $handler);
+    }
+
+    /**
+    * Handle a request
+    */
     public function handle(Http\Request $request) 
     {
         foreach ($this->routes as $route) {
